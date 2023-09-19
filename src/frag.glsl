9dffb013931554cdff796c9b806f334d0de2537a
ihaveapatricklmao <82519853+ihaveapatricklmao@users.noreply.github.com>
2023-09-19 13:18:34 -0400

    slight tweak for debugging
    - might need too make an FBO too debug

--- src/frag.glsl
+++ src/frag.glsl
@@ -1,8 +1,12 @@
 #version 330 core
 
+out vec4 FragColor; 
+
 vec2 texCoords = {0.0, 0.0};
 uniform sampler2D textureSampler;
 
 void main() {
     vec4 color = texture(textureSampler, texCoords);
+
+    FragColor = color;
 }
