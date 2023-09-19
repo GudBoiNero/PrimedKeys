81ed00968f090a76bb46aae42623104428b9d033
Benjamin Champagne <bennyocean@outlook.com>
2023-09-19 09:22:56 -0400

    init

--- src/gui.cpp
+++ src/gui.cpp
@@ -7,62 +7,61 @@
 
 namespace gui
 {
-    void ShowMacroMenu(bool p_open) {
-        ImGui::Begin("MacroMenu", &p_open, default_window_flags);
-        ImGui::GetWindowViewport()->Flags = default_viewport_flags;
+	void ShowMacroMenu(bool p_open) {
+		ImGui::Begin("MacroMenu", &p_open, default_window_flags);
+		ImGui::GetWindowViewport()->Flags = default_viewport_flags;
 
-        {
-            const char img_icon[] = "images/buttons/button.png";
+		{
 
-			gui::TextureButton((ImGuiID)"_pktb1", img_icon, ImGuiButtonFlags_None); ImGui::SameLine();
-			gui::TextureButton((ImGuiID)"_pktb2", img_icon, ImGuiButtonFlags_None); ImGui::SameLine();
-			gui::TextureButton((ImGuiID)"_pktb3", img_icon, ImGuiButtonFlags_None); ImGui::SameLine();
-			gui::TextureButton((ImGuiID)"_pktb4", img_icon, ImGuiButtonFlags_None); 
+			gui::TextureButton((ImGuiID)"_pktb1", nullptr, ImGuiButtonFlags_None); ImGui::SameLine();
+			gui::TextureButton((ImGuiID)"_pktb2", nullptr, ImGuiButtonFlags_None); ImGui::SameLine();
+			gui::TextureButton((ImGuiID)"_pktb3", nullptr, ImGuiButtonFlags_None); ImGui::SameLine();
+			gui::TextureButton((ImGuiID)"_pktb4", nullptr, ImGuiButtonFlags_None);
 
-			gui::TextureButton((ImGuiID)"_pktb5", img_icon, ImGuiButtonFlags_None); ImGui::SameLine();
-			gui::TextureButton((ImGuiID)"_pktb6", img_icon, ImGuiButtonFlags_None); ImGui::SameLine();
-			gui::TextureButton((ImGuiID)"_pktb7", img_icon, ImGuiButtonFlags_None); ImGui::SameLine();
-			gui::TextureButton((ImGuiID)"_pktb8", img_icon, ImGuiButtonFlags_None);
-        }
+			gui::TextureButton((ImGuiID)"_pktb5", nullptr, ImGuiButtonFlags_None); ImGui::SameLine();
+			gui::TextureButton((ImGuiID)"_pktb6", nullptr, ImGuiButtonFlags_None); ImGui::SameLine();
+			gui::TextureButton((ImGuiID)"_pktb7", nullptr, ImGuiButtonFlags_None); ImGui::SameLine();
+			gui::TextureButton((ImGuiID)"_pktb8", nullptr, ImGuiButtonFlags_None);
+		}
 
 		ImGui::End();
 	}
 
-    // https://github.com/ocornut/imgui/wiki/Image-Loading-and-Displaying-Examples#example-for-opengl-users
-    // Simple helper function to load an image into a OpenGL texture with common settings
-    bool LoadTextureFromFile(const char* filename, GLuint* out_texture, int* out_width, int* out_height)
-    {
-        // Load from file
-        int image_width = 0;
-        int image_height = 0;
-        unsigned char* image_data = stbi_load(filename, &image_width, &image_height, NULL, 4);
-        if (image_data == NULL)
-            return false;
+	// https://github.com/ocornut/imgui/wiki/Image-Loading-and-Displaying-Examples#example-for-opengl-users
+	// Simple helper function to load an image into a OpenGL texture with common settings
+	bool LoadTextureFromFile(const char* filename, GLuint* out_texture, int* out_width, int* out_height)
+	{
+		// Load from file
+		int image_width = 0;
+		int image_height = 0;
+		unsigned char* image_data = stbi_load(filename, &image_width, &image_height, NULL, 4);
+		if (image_data == NULL)
+			return false;
 
-        // Create a OpenGL texture identifier
-        GLuint image_texture;
-        glGenTextures(1, &image_texture);
-        glBindTexture(GL_TEXTURE_2D, image_texture);
+		// Create a OpenGL texture identifier
+		GLuint image_texture;
+		glGenTextures(1, &image_texture);
+		glBindTexture(GL_TEXTURE_2D, image_texture);
 
-        // Setup filtering parameters for display
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // This is required on WebGL for non power-of-two textures
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // Same
+		// Setup filtering parameters for display
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // This is required on WebGL for non power-of-two textures
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // Same
 
-        // Upload pixels into texture
+		// Upload pixels into texture
 #if defined(GL_UNPACK_ROW_LENGTH) && !defined(__EMSCRIPTEN__)
-        glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+		glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
 #endif
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
-        stbi_image_free(image_data);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
+		stbi_image_free(image_data);
 
-        *out_texture = image_texture;
-        *out_width = image_width;
-        *out_height = image_height;
+		*out_texture = image_texture;
+		*out_width = image_width;
+		*out_height = image_height;
 
-        return true;
-    }
+		return true;
+	}
 
 	struct TextureButtonData
 	{
@@ -95,7 +94,7 @@ namespace gui
 	}
 
 	// Custom Textures
-	bool TextureButton(ImGuiID id, const char img_icon[], ImTextureID texture_default_id, ImTextureID texture_hover_id, ImTextureID texture_active_id, ImGuiButtonFlags flags)
+	bool TextureButton(ImGuiID id, ImTextureID texture_icon_id, ImTextureID texture_default_id, ImTextureID texture_hover_id, ImTextureID texture_active_id, ImGuiButtonFlags flags)
 	{
 		ImTextureID texture_id = texture_default_id;
 		ImGuiContext& g = *GImGui;
@@ -124,6 +123,8 @@ namespace gui
 			texture_id = texture_active_id;
 		}
 
+		ImVec2 icon_offset = (hovered ? (pressed || held ? ImVec2(0, 2) : ImVec2(0, 1)) : (pressed || held ? ImVec2(0, 2) : ImVec2()));
+
 		// Render
 		//const ImU32 col = ImGui::GetColorU32((held && hovered) ? ImGuiCol_ButtonActive : hovered ? ImGuiCol_ButtonHovered : ImGuiCol_Button);
 		const ImU32 col = ImGui::GetColorU32(ImGuiCol_Button);
@@ -132,13 +133,15 @@ namespace gui
 		if (btn_bg_col.w > 0.0f)
 			window->DrawList->AddRectFilled(bb.Min + padding, bb.Max - padding, ImGui::GetColorU32(btn_bg_col));
 		window->DrawList->AddImage(texture_id, bb.Min + padding, bb.Max - padding, btn_uv0, btn_uv1, ImGui::GetColorU32(btn_tint_col));
+		if (texture_icon_id)
+			window->DrawList->AddImage(texture_icon_id, bb.Min + padding + icon_offset, bb.Max - padding + icon_offset, btn_uv0, btn_uv1, ImGui::GetColorU32(btn_tint_col));
 
 		return pressed;
 	}
 
 	// Native textures
-	bool TextureButton(ImGuiID id, const char img_icon[], ImGuiButtonFlags flags)
+	bool TextureButton(ImGuiID id, ImTextureID texture_icon_id, ImGuiButtonFlags flags)
 	{
-		return TextureButton(id, img_icon, btn_default_data.texture, btn_hover_data.texture, btn_active_data.texture, flags);
+		return TextureButton(id, texture_icon_id, btn_default_data.texture, btn_hover_data.texture, btn_active_data.texture, flags);
 	}
 }
