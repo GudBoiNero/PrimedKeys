e084c4f2f57eb4a960d8e7086a26883ffe6f94f0
Ben Champagne <donkeykong797@gmail.com>
2023-12-15 09:53:18 -0500

    ooooh progress

--- src/win_handler.cpp
+++ src/win_handler.cpp
@@ -17,7 +17,29 @@ LRESULT CALLBACK handler::CBTProc(int nCode, WPARAM wParam, LPARAM lParam)
 
 HHOOK handler::SetHook()
 {
-	return SetWindowsHookEx(WH_CBT, handler::CBTProc, 0, GetCurrentThreadId());
+    HHOOK hook;
+    HWND foregroundWindow = GetForegroundWindow();
+
+    if (foregroundWindow != NULL) {
+        // Get the process ID and thread ID of the foreground window
+        DWORD process_id, thread_id;
+        thread_id = GetWindowThreadProcessId(foregroundWindow, &process_id);
+
+        if (thread_id != 0 && process_id != 0) {
+            std::cout << "Foreground Window Process ID: " << process_id << std::endl;
+            std::cout << "Foreground Window Thread ID: " << thread_id << std::endl;
+
+            hook = SetWindowsHookEx(WH_CBT, handler::CBTProc, 0, thread_id);
+        }
+        else {
+            std::cerr << "Error getting thread/process ID" << std::endl;
+        }
+    }
+    else {
+        std::cerr << "Error getting foreground window" << std::endl;
+    }
+
+	return hook;
 }
 
 void handler::Unhook(HHOOK hookHandle)
