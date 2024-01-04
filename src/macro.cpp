7de692d2acc29cdb590ba8747814a18ba501cc49
Benjamin Champagne <donkeykong797@gmail.com>
2024-01-04 17:05:52 -0500

    method bodies

--- src/macro.cpp
+++ src/macro.cpp
@@ -13,15 +13,31 @@ MacroFile MacroManager::GetMacroFile() noexcept
 MacroFile MacroManager::LoadMacroFile(const std::string& file_path) noexcept 
 {
 	std::fstream f(file_path);
-	std:: stringstream iss;
+	std::stringstream iss;
 	iss << f.rdbuf();
 	std::string data = iss.str();
 
-	MacroFile macro_file = jsoncons::decode_json<MacroFile>(data);
+	MacroFile macro_file;
+	
+	try {
+		macro_file = jsoncons::decode_json<MacroFile>(data);
+	}
+	catch(jsoncons::conv_error err) {}
+
 	return macro_file;
 }
 
 void MacroManager::WriteMacroFile(const MacroFile& macro_file, const std::string& file_path) noexcept 
 {
-	
+	std::string data;
+
+	try {
+		jsoncons::encode_json<MacroFile>(macro_file, data, jsoncons::indenting::indent);
+
+		// Only write data if it's successfully encoded.
+		std::ofstream out(file_path);
+		out << data;
+		out.close();
+	}
+	catch(jsoncons::conv_error) {}
 }
